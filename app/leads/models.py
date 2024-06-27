dd23657bf47924c3b0f29c534ffce1b9ba80da5a
hammadpasha12 <hammadqsol@gmail.com>
2024-06-27 14:05:02 +0500

    create leads service

---
+++ app/leads/models.py
@@ -0,0 +1,26 @@
+import datetime as _dt
+from datetime import date
+import sqlalchemy as _sql
+import sqlalchemy.orm as _orm
+import app.core.db.session as _database
+import bcrypt as _bcrypt
+import sqlalchemy.ext.declarative as _declarative
+
+class Leads(_database.Base):
+    _tablename = "leads"
+    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    first_name = _sql.Column(_sql.String)
+    last_name = _sql.Column(_sql.String)
+    email = _sql.Column(_sql.String)
+    mobile = _sql.Column(_sql.String)
+    phone = _sql.Column(_sql.String)
+    staff_id = _sql.Column(_sql.Integer)
+    status = _sql.Column(_sql.String)
+    source_id = _sql.Column(_sql.Integer)
+    lead_since = _sql.Column(_sql.Date)
+    notes=_sql.Column(_sql.String,nullable=True)
+    created_at= _sql.Column(_sql.DateTime,default=date.datetime.now())
+    updated_at= _sql.Column(_sql.DateTime,default=date.datetime.now())
+    created_by= _sql.Column(_sql.Integer)
+    updated_by= _sql.Column(_sql.Integer)
+    is_deleted= _sql.Column(_sql.Boolean, default=False)
