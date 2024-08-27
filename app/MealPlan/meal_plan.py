8bc139e56b69caec42cd3386b5c7d0756f0f2d1e
MuhammadMillwala53 <muhammadmillwala.qsols@gmail.com>
2024-08-27 12:25:04 +0500

    Meal plan finalised

--- app/MealPlan/meal_plan.py
+++ app/MealPlan/meal_plan.py
@@ -75,9 +75,10 @@ async def update_meal_plan(meal_plan: _schemas.UpdateMealPlan,request:Request,db
    
 
 @router.delete("/meal_plans/{id}", response_model=_schemas.ReadMealPlan)
-async def delete_meal_plan(id:int, db: _orm.Session = Depends(get_db)):
+async def delete_meal_plan(id:int, request:Request, db: _orm.Session = Depends(get_db)):
     try:
-        deleted_meal_plan = _service.delete_meal_plan(id, db)
+        user_id = request.state.user.get('id')
+        deleted_meal_plan = _service.delete_meal_plan(id, user_id, db)
         if deleted_meal_plan is None:
             raise HTTPException(status_code=404, detail="Meal plan not found")
         return deleted_meal_plan
