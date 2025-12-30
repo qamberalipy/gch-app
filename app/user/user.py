08b8db52fac1d12824119020d7c29cb3aa7b1e2e
Qamber ali <qamber246@gmail.com>
2025-12-30 16:52:34 +0500

    api update

--- app/user/user.py
+++ app/user/user.py
@@ -89,7 +89,7 @@ async def delete_user(
 async def get_all_users(
     skip: int = 0,
     limit: int = 100,
-    role: Optional[_models.UserRole] = None, 
+    role: Optional[str] = None, 
     search: Optional[str] = None,
     current_user: _models.User = Depends(get_admin_or_manager),
     db: Session = Depends(_services.get_db)
